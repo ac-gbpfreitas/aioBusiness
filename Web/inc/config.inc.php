9e53c7f548d9c52821573740016a771fb27c0841
Gustavo Freitas <70547481+ac-gbpfreitas@users.noreply.github.com>
2021-12-16 02:33:00 -0800

    Files reuploaded

--- Web/inc/config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$today = date("Y-m-d");
-define("DATA_OUTPUT","inc/data/".$today.".csv");
-
-define("MONGO_DB_USER","admin");
-define("MONGO_DB_PASS","admin");
-define("MONGO_DB_NAME","aioRestaurants");
-define("MONGO_DB_HOST","@cluster0.usuw1.mongodb.net");
