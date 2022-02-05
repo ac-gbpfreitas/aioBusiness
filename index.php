8e1caac9352eb9e4ada7b1ecfa39f37245a91939
Gustavo Freitas <70547481+ac-gbpfreitas@users.noreply.github.com>
2022-02-04 19:48:21 -0800

    Update index.php

--- index.php
+++ index.php
@@ -25,11 +25,10 @@ if(!empty($_GET["page"])){
     Test Connection
     */
     $newCon = new PDOMongo("employee");
-    $newCon->bindElement("employeeId",3);
-    var_dump(method_exits($newCon,"bindElement"));
-    /*
+    $newCon::bindElement("employeeId",3);
+    
     var_dump($newCon::findData([],1));
-    */
+    
     if($_GET["page"] == "dashboard"){
 
         Page::pageLeftMenu();
